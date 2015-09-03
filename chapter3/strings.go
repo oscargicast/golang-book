96d2ed8333c2eba56526d756ad47b9dd96b6b182
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2015-09-03 01:34:26 -0500

    [feature/#2] String types

---
+++ chapter3/strings.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(len("Hello World"))
+	// Returns decimal representation of 'e'.
+	fmt.Println("Hello World"[1])
+	fmt.Println("Hello " + "World")
+}
