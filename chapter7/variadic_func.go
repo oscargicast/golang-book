f7920408dac2f4e577224f0ae646ee257ac6afb1
Oscar Giraldo Castillo <oscar.gi.cast@gmail.com>
2015-09-14 21:45:42 +0000

    [feature/#6] Passing a slice to a variadic function

--- chapter7/variadic_func.go
+++ chapter7/variadic_func.go
@@ -14,4 +14,7 @@ func main() {
 	fmt.Println(add(1, 2, 3, 4, 5, 6))
 	fmt.Println(add(1, 3, 4))
 	fmt.Println(add())
+	// Passing a slice of ints.
+	xs := []int{100, 200, 300}
+	fmt.Println(add(xs...))
 }
